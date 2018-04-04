ab5ad34ff2af316b6ce885bbd2216efb08b8785c
Diogo1995 <diogofilipe_10-1@hotmail.com>
2018-04-04 15:29:54 +0100

    Login terminado
    falta registar e menus seguintes!

--- Loja.java
+++ Loja.java
@@ -70,7 +70,9 @@ public class Loja {
 				System.out.println(nif);
 				if(login(nif).equals("Cliente")){
 					menuCliente(sc, nif);
-				}else if(login(nif).equals("Funcionário")) menuFuncionario(sc, nif);
+				}else if(login(nif).equals("Loja")) menuFuncionarioLoja(sc, nif);
+				else if(login(nif).equals("Caixa")) menuFuncionarioLoja(sc, nif);
+				
 				break;
 				
 			case '2':
@@ -133,21 +135,15 @@ public class Loja {
 	}
 	
 	
-	public void menuFuncionario(Scanner sc, String nif) {
+	public void menuFuncionarioCaixa(Scanner sc, String nif) {
 		char op;
 		do {
 			System.out.println();
 			System.out.println();
-			System.out.println("*** Catalogo Loja ***");
+			System.out.println("*** Funcionário Caixa ***");
 			System.out
-					.println("1 - .");
-			System.out
-					.println("2 - .");
-			System.out
-					.println("3 - .");
-			System.out.println("4 - ");
-			System.out.println("5 - .");
-			System.out.println("6 - Terminar Sessão.");
+					.println("1 - Consultar carrinhos de compras por aprovar.");
+			System.out.println("2 - Terminar Sessão.");
 			System.out.println("0 - Terminar!");
 			String str = sc.nextLine();
 			if (str != null && str.length() > 0)
@@ -160,7 +156,49 @@ public class Loja {
 				//apresenta();
 				break;
 				
-			case '6':
+			case '2':
+				menuAcesso();
+				break;
+				
+			case '0':
+				break;
+			default:
+				System.out.println("Opção inválida, esolha uma opção do menu.");
+			}
+		} while (op != '0');
+		sc.close();
+		System.out.println("Terminou a execução.");
+		System.exit(0);
+	}
+	
+	
+	
+	public void menuFuncionarioLoja(Scanner sc, String nif) {
+		char op;
+		do {
+			System.out.println();
+			System.out.println();
+			System.out.println("*** Funcionário Loja ***");
+			System.out
+					.println("1 - Adicionar nova peça.");
+			System.out
+					.println("2 - Modificar preço de peça existente.");
+			System.out
+					.println("3 - Modificar quantidade de peça existente.");
+			System.out.println("4 - Terminar Sessão.");
+			System.out.println("0 - Terminar!");
+			String str = sc.nextLine();
+			if (str != null && str.length() > 0)
+				op = str.charAt(0);
+			else
+				op = ' ';
+			switch (op) {
+			
+			case '1':
+				//apresenta();
+				break;
+				
+			case '4':
 				menuAcesso();
 				break;
 				
@@ -218,7 +256,10 @@ public class Loja {
 		NodeList nodesUtilizador = Utilizadores.getDocumentElement().getElementsByTagName("Utilizador");
 		for(int i = 0; i < nodesUtilizador.getLength(); i++) {
 			if(nodesUtilizador.item(i).getAttributes().getNamedItem("NIF").getTextContent().equals(nif)) {
-				return nodesUtilizador.item(i).getChildNodes().item(1).getNodeName();
+				if( nodesUtilizador.item(i).getChildNodes().item(1).getNodeName().equals("Funcionário")) {
+					return nodesUtilizador.item(i).getAttributes().getNamedItem("local").getTextContent();
+				}
+				else return nodesUtilizador.item(i).getChildNodes().item(1).getNodeName();
 			}
 		}
 		return null;
